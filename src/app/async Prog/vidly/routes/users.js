8dabf049f55db38d41b92208114e683b99024458
muhammad-muzzammil <muhammadmuzzammil10@hotmail.com>
2019-01-10 20:06:11 +0500

    commit

--- src/app/async Prog/vidly/routes/users.js
+++ src/app/async Prog/vidly/routes/users.js
@@ -1,10 +1,17 @@
+const auth = require("../middleware/auth");
 const bcrypt = require("bcrypt");
 const { validate, User } = require("../models/user");
 const express = require("express");
 const router = express.Router();
 const _ = require("lodash");
 
-router.post("/", async (req, res) => {
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id)
+  .select("-password");
+  res.send(user)
+});
+
+router.post("/", async (req, res) => { // Register User
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -16,23 +23,21 @@ router.post("/", async (req, res) => {
   //   email: req.body.email,
   //   password: req.body.password
   // });
-
-  
   user = new User(_.pick(req.body, ["name", "email", "password"]));
 
-  
-
-
+  const token = user.generateAuthToken();
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
+  // user.token = token
   await user.save();
 
   // res.send({ this approach is good but we'll prefer lodash
   //   name:user.name,
   //   email:user.email
   // });
-  const token = user.generateAuthToken();
-  res.header('x-auth-token',token).send(_.pick(user, ["_id", "name", "email"]));  //  Lodash
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "name", "email"])); //  Lodash
 });
 
 module.exports = router;
